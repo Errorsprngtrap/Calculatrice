eac7b310e5e6b60748d15278e82022f142a51860
Spring <baptistounet06@gmail.com>
2026-08-18 12:14:09 +0200

    v1 for now should be doen

--- main.cpp
+++ main.cpp
@@ -44,8 +44,8 @@ int main(int argc, char* argv[]) {
         double numberOne = forceAnswer();
         double numberTwo = forceAnswer();
         char operatorSign = forceOperatorAnswer();
-
         double result;
+        bool errorSkipped = false;
 
         switch (operatorSign) {
             case '+':
@@ -66,10 +66,13 @@ int main(int argc, char* argv[]) {
                 break;
             default:
                 std::cout << "Invalid operator" << std::endl;
+                errorSkipped = true;
                 break;
         }
 
-        std::cout << result << std::endl;
+        if (errorSkipped == true)  {
+            std::cout << result << std::endl;
+        }
 
         std::cout << "Press y if you want to do more maths" << std::endl;
 
@@ -81,58 +84,4 @@ int main(int argc, char* argv[]) {
         }
 
     }
-
-    // float numberOne;
-    // float numberTwo;
-    // char operatorSign;
-    // bool continueLoop = true;
-    //
-    // std::cout << "Please enter a number" << std::endl;
-    // std::cin >> numberOne;
-    //
-    // if (std::cin.fail()) {
-    //     std::cin.clear();
-    // }
-    //
-    // std::cout << "Please enter an operator" << std::endl;
-    // std::cin >> operatorSign;
-    //
-    // if (std::cin.fail()) {
-    //     std::cin.clear();
-    // }
-    //
-    // std::cout << "Please enter a second number" << std::endl;
-    // std::cin >> numberTwo;
-    //
-    // if (std::cin.fail()) {
-    //     std::cin.clear();
-    // }
-    //
-    // float result = 0;
-    //
-    // switch (operatorSign) {
-    //     case '+':
-    //         result = numberOne + numberTwo;
-    //         break;
-    //     case '-':
-    //         result = numberOne - numberTwo;
-    //         break;
-    //     case '*':
-    //         result = numberOne * numberTwo;
-    //         break;
-    //     case '/':
-    //         if (numberTwo == 0) {
-    //             std::cout << "Division by zero unable to do that" << std::endl;
-    //         }else {
-    //             result = numberOne / numberTwo;
-    //         }
-    //         break;
-    //     default:
-    //         std::cout << "Invalid operator" << std::endl;
-    //         break;
-    // }
-    //
-    // std::cout << result << std::endl;
-    //
-    // return 0;
 }
