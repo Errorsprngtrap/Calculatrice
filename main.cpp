b9817a9908ad9d545fd3fd05f779ecc1a0989f88
Spring <baptistounet06@gmail.com>
2026-08-18 11:53:05 +0200

    v1 for now

--- main.cpp
+++ main.cpp
@@ -4,15 +4,28 @@ int main(int argc, char* argv[]) {
     float numberOne;
     float numberTwo;
     char operatorSign;
+    bool continueLoop = true;
 
     std::cout << "Please enter a number" << std::endl;
     std::cin >> numberOne;
 
+    if (std::cin.fail()) {
+        std::cin.clear();
+    }
+
     std::cout << "Please enter an operator" << std::endl;
     std::cin >> operatorSign;
 
+    if (std::cin.fail()) {
+        std::cin.clear();
+    }
+
     std::cout << "Please enter a second number" << std::endl;
     std::cin >> numberTwo;
+
+    if (std::cin.fail()) {
+        std::cin.clear();
+    }
 
     float result = 0;
 
@@ -27,11 +40,14 @@ int main(int argc, char* argv[]) {
             result = numberOne * numberTwo;
             break;
         case '/':
-            if (numberOne == 0||numberTwo == 0) {
+            if (numberTwo == 0) {
                 std::cout << "Division by zero unable to do that" << std::endl;
             }else {
                 result = numberOne / numberTwo;
             }
+            break;
+        default:
+            std::cout << "Invalid operator" << std::endl;
             break;
     }
 
